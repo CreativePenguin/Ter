6f7bb52d38419e117e29556a8b0cfd16f39bfe5a
Winston Peng <panda501wp@gmail.com>
2022-02-22 22:47:11 -0500

    user stays logged in
    Just casually automating one more task so that less working class people get paid, haha social commentary

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -18,11 +18,18 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if(UserDefaults.standard.bool(forKey: "userLoggedIn")) {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
     @IBAction func onLoginButton(_ sender: Any) {
         let myUrl = "https://api.twitter.com/oauth/request_token"
 
-        print("hello world, time to do some killing :D")
+//        print("hello world, time to do some killing :D")
         TwitterAPICaller.client?.login(url: myUrl, success: {
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("could not log in")
