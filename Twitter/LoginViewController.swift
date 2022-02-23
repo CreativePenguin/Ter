b38c95a68aee13362313edb0a03769c2725f45a6
Winston Peng <panda501wp@gmail.com>
2022-02-22 22:30:32 -0500

    Add logout function
    Because congress logs out of doing anything lol XD. I'm hilarious.

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,7 +19,14 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func onLoginButton(_ sender: Any) {
+        let myUrl = "https://api.twitter.com/oauth/request_token"
+
         print("hello world, time to do some killing :D")
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("could not log in")
+        })
     }
 
     /*
