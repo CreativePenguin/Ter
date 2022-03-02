180d004138a49ccb6e423d16a3ebb84b5cffc780
Winston Peng <panda501wp@gmail.com>
2022-03-01 22:25:17 -0500

    add new tweet screen

---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Winston Peng on 3/1/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func tweet(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
